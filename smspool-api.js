09c5cd92062a0fe9584bc5005f6d3660fede0486
Red <godsamomen@protonmail.com>
2024-04-25 16:23:02 +0300

    Update smspool-api.js

--- smspool-api.js
+++ smspool-api.js
@@ -8,6 +8,7 @@ class SmsPool {
         this.apiKey = apiKey;
     }
 
+    //information fonksiyonlari
     async getBalance() {
         const url = 'https://api.smspool.net/request/balance';
         const headers = {
@@ -25,8 +26,52 @@ class SmsPool {
         }
     }
 
+    async getSuccessRate(service) {
+        const url = 'https://api.smspool.net/request/success_rate';
+        const headers = {
+            'Authorization': `Bearer ${this.apiKey}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+        const data = new URLSearchParams({ service });
+    
+        try {
+            const response = await axios.post(url, data, { headers });
+            return response.data;
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    }
+
+    async getSuggestedCountries(service) {
+        const url = `https://api.smspool.net/request/suggested_countries?service=${service}`;
+        const headers = {
+            'Authorization': `Bearer ${this.apiKey}`
+        };
+    
+        try {
+            const response = await axios.get(url, { headers });
+            return response.data;
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    }
+    
     async getCountryList() {
         const url = 'https://api.smspool.net/country/retrieve_all';
+        const headers = {
+            'Authorization': `Bearer ${this.apiKey}`
+        };
+    
+        try {
+            const response = await axios.get(url, { headers });
+            return response.data;
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    }
+
+    async getServiceList(country) {
+        const url = `https://api.smspool.net/service/retrieve_all?country=${country}`;
         const headers = {
             'Authorization': `Bearer ${this.apiKey}`
         };
@@ -158,6 +203,39 @@ class SmsPool {
             length: length,
             search: search
         };
+    
+        try {
+            const response = await axios.post(url, data, { headers });
+            return response.data;
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    }
+
+    //Quick Price
+    async getPricing(country) {
+        const url = 'https://api.smspool.net/request/pricing';
+        const headers = {
+            'Authorization': `Bearer ${this.apiKey}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+        const data = new URLSearchParams({ country });
+    
+        try {
+            const response = await axios.post(url, data, { headers });
+            return response.data;
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    }
+
+    async getPriceAndSuccessRate(country, service) {
+        const url = 'https://api.smspool.net/request/price';
+        const headers = {
+            'Authorization': `Bearer ${this.apiKey}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+        const data = new URLSearchParams({ country, service });
     
         try {
             const response = await axios.post(url, data, { headers });
